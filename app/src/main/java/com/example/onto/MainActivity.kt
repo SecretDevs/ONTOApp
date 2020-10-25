55948daf3b3b1f3d51aec7f135111a8dfcd313d6
Anton <leeeloooda@gmail.com>
2020-10-25 20:54:56 +0300

    add bottom navigation and button gradient

--- app/src/main/java/com/example/onto/MainActivity.kt
+++ app/src/main/java/com/example/onto/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.onto
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.onto.maps.MapsFragment
 import com.example.onto.products.ProductsFragment
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
+import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -12,11 +15,52 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(savedInstanceState == null) { // initial transaction should be wrapped like this
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, ProductsFragment())
-                .commitAllowingStateLoss()
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.page_map -> {
+                    if (LAST_ITEM != R.id.page_map) {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container, MapsFragment())
+                            .commitAllowingStateLoss()
+                        LAST_ITEM = it.itemId
+                        true
+                    } else {
+                        false
+                    }
+                }
+                R.id.page_discount -> {
+                    false
+                }
+                R.id.page_shop -> {
+                    if (LAST_ITEM != R.id.page_shop) {
+                        supportFragmentManager.beginTransaction()
+                            .replace(R.id.fragment_container, ProductsFragment())
+                            .commitAllowingStateLoss()
+                        LAST_ITEM = it.itemId
+                        true
+                    } else {
+                        false
+                    }
+                }
+                R.id.page_articles -> {
+                    false
+                }
+                R.id.page_user -> {
+                    false
+                }
+                else -> {
+                    Timber.e("Unknown item id ${it.itemId}")
+                    false
+                }
+            }
         }
-
+        if (savedInstanceState == null) {
+            bottom_navigation.findViewById<View>(R.id.page_shop).performClick()
+        }
     }
+
+    companion object {
+        private var LAST_ITEM: Int = 0
+    }
+
 }
